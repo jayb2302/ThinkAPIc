449c30e8e611a3f6e26a36b4aaac7855d6bc90a6
jayb2302 <jonbja01@easv365.dk>
2025-02-09 21:21:51 +0100

    🛠️ chore(api): add CORS support for frontend access
    - Install and Configure CORS middleware.
    - Add support for specific origins, methods and headers.
    - Update àpp.ts`to include cors() with dynamic origin.
    
    Allows the frontend to communicate with the API

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import dotenvFlow from 'dotenv-flow';
+import cors from 'cors';
 import { connectDB } from "./config/database";
 
 import routes from './routes';
@@ -9,6 +10,15 @@ dotenvFlow.config();
 // Create Express Application
 const app: Application = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Allow Frontend access
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}));
 
 // Connect to MongoDB
 connectDB();
