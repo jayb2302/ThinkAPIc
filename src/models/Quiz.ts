fd302b4540a8e0619d2f43aae6c6ae47880a791d
jayb2302 <jonbja01@easv365.dk>
2025-03-09 21:06:12 +0100

    🐛 fix(error-handling): improve validation, error handling, and quiz order management
    - Reuse `validateQuizFields` inside `validateQuizUpdate` to avoid duplicated logic in `validateQuizData` and `validateQuizUpdate`.
    - Merge `validateQuizData` and `validateQuizUpdate` into a single `validateQuiz` function with an `isUpdate` flag to handle both creation and updates.
    - Prevent quiz update from reducing options to just one by reusing validation logic.
    - Refactor `getQuizzesByTopic` to throw a `404` error when no quizzes are found.
    - Move validation logic from `quizController` to `quizService` for better separation of concerns.
    - Add `QuizSchema.pre("save")` hook to let MongoDB handle the quiz options' order.
    - Update user authentication to return a `401` status code for invalid credentials instead of `500`.
    - Add `await checkCourseExists(courseId)` in `getUserQuizProgress` to validate course existence.
    - Refine error handling in `errorHandlers.ts` to use dynamic status codes based on the error (`err.status`) instead of always returning `500`.

--- src/models/Quiz.ts
+++ src/models/Quiz.ts
@@ -5,13 +5,24 @@ import { IQuiz, IQuizOption } from "../interfaces/IQuiz";
 const QuizOptionSchema = new Schema<IQuizOption>({
     text: { type: String, required: true },
     isCorrect: { type: Boolean, required: true, default: false },
-    order: { type: Number, required: true }
+    order: { type: Number }
 });
 
 const QuizSchema = new Schema<IQuiz>({
     topic: { type: Schema.Types.ObjectId, ref: "Topic", required: true },
-    question: { type: String, required: true },
+    question: { type: String, unique: true, required: true },
     options: { type: [QuizOptionSchema], required: true }
 }, { timestamps: true });
+
+QuizSchema.pre("save", function (next) {
+    if (this.isModified("options")) {
+        this.options.forEach((option: IQuizOption, index: number) => {
+            if (option.order === undefined) {
+                option.order = index + 1; 
+            }
+        });
+    }
+    next();
+});
 
 export default mongoose.model<IQuiz>("Quiz", QuizSchema);
