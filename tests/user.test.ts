2518d9b384cc1e096a18bdf94a58573bfd623fe0
jayb2302 <jonbja01@easv365.dk>
2025-05-28 20:27:25 +0200

    ✅ test(auth): add valid and invalid login tests to user.test.ts

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -10,9 +10,9 @@ export default function userTestCollection() {
       email: "john@example.com",
       password: "password",
     };
-    
+
     //Act
-    const response = await request.post("/api/auth/register", {data: user});
+    const response = await request.post("/api/auth/register", { data: user });
     const json = await response.json();
 
     //Assert
@@ -29,14 +29,43 @@ export default function userTestCollection() {
       email: "john@example.com",
       password: "pass", //invalid passwod according to Joi schema
     };
-    
+
     //Act
-    const response = await request.post("/api/auth/register", {data: user});
+    const response = await request.post("/api/auth/register", { data: user });
     const json = await response.json();
 
     //Assert
     expect(response.status()).toBe(400);
     //console.log(json.error)
-    expect(json.error).toEqual("Validation error: \"password\" length must be at least 6 characters long");
+    expect(json.error).toEqual(
+      'Validation error: "password" length must be at least 6 characters long'
+    );
+  });
+
+  test("Valid login", async ({ request }) => {
+    const user = {
+      username: "John Doe",
+      email: "john@example.com",
+      password: "password",
+    };
+
+    // Register the user
+    await request.post("/api/auth/register", { data: user });
+
+    // Attempt login
+    const credentials = { email: user.email, password: user.password };
+    const response = await request.post("/api/auth/login", {
+      data: credentials,
+    });
+
+    expect(response.status()).toBe(200);
+  });
+
+  test("Invalid login", async ({ request }) => {
+    const credentials = { email: "john@example.com", password: "wrongpass" };
+    const response = await request.post("/api/auth/login", {
+      data: credentials,
+    });
+    expect(response.status()).toBe(401);
   });
 }
