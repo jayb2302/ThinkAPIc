32c3d8385a77f1cfb1adcb81f7d590d7540cf425
Jay Brilliant Beaver <118164561+jayb2302@users.noreply.github.com>
2025-05-30 21:56:44 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 const allowedOrigins = [
   process.env.CLIENT_URL,
   'http://localhost:5173',
-  'http://localhost:3000',
+  'http://localhost:3000'
 ];
 
 app.use(
